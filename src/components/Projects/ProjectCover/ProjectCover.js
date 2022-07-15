1e7db2b79ed011bcdfccde2ca4309d9e4c2165d5
Ri-yan <https://github.com/Ri-yan>
2022-07-15 12:06:04 +0530

    fixes

--- src/components/Projects/ProjectCover/ProjectCover.js
+++ src/components/Projects/ProjectCover/ProjectCover.js
@@ -7,11 +7,11 @@ const ProjectCover=()=>{
 				<img className='coverimg' src={projectCover}  alt="programmer"/>
 				<div className='title'>
 					<div className='title-name'>
-						My Projects;<div className='nav-links ch' style={{'font-size':'1.5vw'}} onClick={()=>{window.scroll({
+						My Projects;<div className='nav-links ch'  onClick={()=>{window.scroll({
   top: 700,
   left: 0,
   behavior: 'smooth'
-});}}><a href='#' className='ch' style={{'textDecoration':'none',color:'black'}}>Click Here</a></div>
+});}}><a href='#' className='' style={{'textDecoration':'none',color:'black'}}>Click Here</a></div>
 					</div>
 				</div>			
 			</div>
